19a8625a8b80c343e85021ea746016a9e5d2b335
Burton John Samograd <burtonjohnsamograd@protonmail.com>
2022-10-19 21:57:42 -0600

    Updated frame delay time.

--- record-window.sh
+++ record-window.sh
@@ -3,11 +3,13 @@
 id=$(xwininfo | grep id: | while read junk junk2 idcolon id xs; do echo $id; done)
 frame=1
 
+echo "Hit ^C (Control C) to stop recording..."
+
 while true; do
       echo -n .
       xwd -id ${id} > tmp/${frame}.xwd
       convert tmp/${frame}.xwd var/${frame}.jpg
       frame=$((frame+1))
-      sleep .01
+      sleep .17
 done
 
